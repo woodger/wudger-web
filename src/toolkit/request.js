983467ca0f49e5588757821544d3f48cb4dfe2d0
Stanislav Potemkin <woodger@ya.ru>
2020-12-04 13:19:52 +0300

    Fix request counter rule

--- src/toolkit/request.js
+++ src/toolkit/request.js
@@ -64,7 +64,7 @@ export default async function request(...args) {
       body
     });
 
-    if (res.statusText === 'JWT Expired' && counter === 1) {
+    if (res.statusText === 'JWT Expired' && counter <= 2) {
       const opie = getItem('refreshToken');
 
       const res = await throttle(`/api/v1/oauth/${opie}`, {
