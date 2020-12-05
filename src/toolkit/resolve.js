acdb487fb91a9fd83e6c63af809761c456cddc50
Stanislav Potemkin <woodger@ya.ru>
2020-12-05 13:27:42 +0300

    Fix URLs resolver

--- src/toolkit/resolve.js
+++ src/toolkit/resolve.js
@@ -1,8 +1,16 @@
-export default function resolve(src, query = {}) {
-  const url = new URL(src, process.env.API_URL);
+export default function resolve(src, query) {
+  let api;
 
-  for (let i of Object.keys(query)) {
-    url.searchParams.set(i, query[i]);
+  if (process.env.NODE_ENV === 'development') {
+    api = process.env.API_URL;
+  }
+
+  const url = new URL(src, api);
+
+  if (query) {
+    for (let i of Object.keys(query)) {
+      url.searchParams.set(i, query[i]);
+    }
   }
 
   return url;
